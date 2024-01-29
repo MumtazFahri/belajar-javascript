60a3ea401588ecc22ed4a4e24d9021702243f9c0
Mumtaz <verelrizky07@gmail.com>
2024-01-29 14:05:09 +0700

    Pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,10 @@
+var x = prompt("Masukkan Angka");
+//alert("Selamat Datang " + nama);
+
+if(x % 2 == 0) {
+    alert (x + " bilangan genap");
+} else if(x % 2 == 1){
+    alert (x + " bilangan ganjil");
+}else {
+    alert("Angka Woy");
+}
