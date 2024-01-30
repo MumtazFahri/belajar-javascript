c334a29bdd29d174adb446a8f28c0587685e3222
Mumtaz <verelrizky07@gmail.com>
2024-01-30 08:27:44 +0700

    Object

--- javascript-dasar/Latihan8C.js
+++ javascript-dasar/Latihan8C.js
@@ -1,6 +1,10 @@
 var pilihanUser = prompt("Apakah kamu memilih Gajah, Semut, atau Manusia?");
 var pilihanComputer = Math.random();
 
+if(!["Gajah", "Semut", "Manusia"].includes( pilihanUser)) {
+    alert("Yang bener dong")
+}
+
 if(pilihanComputer <0.34) {
     pilihanComputer = "Gajah";
 }else if (pilihanComputer > 0.34 && pilihanComputer <=0.67) {
@@ -9,21 +13,16 @@ if(pilihanComputer <0.34) {
     pilihanComputer = "Semut";
 }
 
-// Proses permainan
-console.log("Pilihan pemain: " + pilihanUser);
-console.log("Pilihan komputer: " + pilihanComputer);
-
 // Hasil permainan
 var hasilPermainan = "";
 if (pilihanUser == pilihanComputer) {
     hasilPermainan = "Seri";
-} else if ((pilihanUser == "Gajah" && pilihanComputer == Math.random) ||
-           (pilihanUser == "Semut" && pilihanComputer == Math.random) ||
-           (pilihanUser == "Manusia" && pilihanComputer == Math.random)) {
+} else if ((pilihanUser == "Gajah" && pilihanComputer == "Semut") ||
+           (pilihanUser == "Semut" && pilihanComputer == "Manusia") ||
+           (pilihanUser == "Manusia" && pilihanComputer == "Gajah")) {
     hasilPermainan = "Pemain menang";
 } else {
     hasilPermainan = "Komputer menang";
 }
 alert("Hasil permainan: " + hasilPermainan);
-
 
